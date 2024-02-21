b5b70c5849a1d2b3985e52dac6a782e03f99b4d4
MarianIsrael <marianwadia55@gmail.com>
2024-02-21 17:40:26 +0200

    connected api for getting profile data

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,39 @@
+"use client"
+import { useCallback, useEffect, useState } from "react"
 import MainNavBar from "./MainNavBar"
 import MainSideBar from "./MainSideBar"
+import axios from "axios"
 
 const Layout = ({ children }) => {
+  const [profileData, setProfileData] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fetchProfileData = useCallback(async()=>{
+    try {
+      setIsLoading(true)
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API}/profile`,{
+        withCredentials: true,
+        headers:{'Content-Type': 'application/json'}
+      })
+      const data = await response.data
+      setProfileData(data)
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setIsLoading(false)
+    }
+  },[])
+
+  useEffect(()=>{
+    fetchProfileData()
+    console.log(profileData)
+  },[fetchProfileData, profileData])
+
   return (
     <div className="flex flex-row w-screen h-screen">
         <MainSideBar />
         <div className="flex-1 flex flex-col overflow-hidden">
-            <MainNavBar />
+            <MainNavBar profileData={profileData} />
             <main className="bg-gray-100 overflow-y-auto flex-1 flex flex-col ">{children}</main>
         </div>
     </div>
